170538e148385b585c7b7f8365273aa10ccbf2c9
VIVEK RAJ <40078116+vivek1125@users.noreply.github.com>
2020-06-28 17:15:38 +0530

    Add files via upload

---
+++ TestAppClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp1
+{
+    class TestAppClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
